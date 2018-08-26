c64ae3481818cabe8c3b5ae2083335133de50caa
Paul Luu <paul.son.luu@gmail.com>
2018-08-26 03:25:42 -0700

    add views and urls

--- blog/forms.py
+++ blog/forms.py
@@ -8,9 +8,18 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('author','title','text')
 
+        widgets = {
+            'title' : forms.TextInput(attrs={'class':'textinputclass'}),
+            'text' : forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'})
+        }
+
 class CommentForm(forms.ModelForm):
 
     class Meta():
         model = Comments
         fields = ('author','text')
-        
+
+        widgets = {
+            'author' : forms.TextInput(attrs={'class':'textinputclass'}),
+            'text' : forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'})
+        }
--- mysite/settings.py
+++ mysite/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR,'blog/templates/blog')
 
 
 # Quick-start development settings - unsuitable for production
@@ -50,12 +51,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'django_blog.urls'
+ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'django_blog.wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 # Database
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+LOGIN_REDIRECT_URL = '/'
